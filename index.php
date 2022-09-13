e0cad9b76b64f2909a0f5b06b6fed3dceb8f624d
francis556 <vikrammehra244@gmail.com>
2022-09-13 07:57:24 +0200

    first php tag

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+	<h1>
+		<?php echo "Hello World Heading"; ?>
+	</h1>
+	<p>
+		
+		<?php echo "I am learning to code in php!! Hurrey..." ?>
+
+
+	</p>
+</body>
+</html>
