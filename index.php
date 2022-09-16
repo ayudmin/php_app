59739051e01f353e171af0cfdb4a726b4bb23a60
francis556 <vikrammehra244@gmail.com>
2022-09-16 10:59:08 +0200

    conditionals and boolean

--- index.php
+++ index.php
@@ -4,18 +4,38 @@
 	<meta charset="utf-8">
 	<title></title>
 </head>
+<style type="text/css">
+	
+	body {
+
+	    display: grid;
+	    place-items: center;
+	    height: 100vh;
+	    font-family: sans-serif;
+	    margin: 0
+	}
+
+</style>
 <body>
+
+	<?php 
+
+		$name = '"Dark Matter"';
+		$read = false;
+
+		if ($read){
+
+			$message = "You have read $name.";
+		} else {
+
+			$message = "You have NOT read $name.";
+		}
+
+	 ?>
 
 	<h1>
 
-		<?php 
-
-			$greeting = "Hello";
-
-			echo "$greeting Everybody!";
-
-
-		 ?>
+		<?= $message ?>
 
 	</h1>
 
