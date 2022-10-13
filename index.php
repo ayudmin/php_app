a46e278f32342c9f3671684600b555762241fa9f
francis556 <vikrammehra244@gmail.com>
2022-10-13 15:02:22 +0200

    update file for project rebase

--- index.php
+++ index.php
@@ -1,40 +1,5 @@
 <?php 
 
-$books = [
-
-	[
-		"name" => "Biology",
-		"category" => "Science",
-		"yearReleased" => "2000",
-		"purchaseUrl" => "http.example.com"
-
-	],
-
-	[
-		"name" => "History",
-		"category" => "Arts",
-		"yearReleased" => "1902",
-		"purchaseUrl" => "http.example.com"
-
-	],
-
-	[
-		"name" => "Mathematics",
-		"category" => "Science",
-		"yearReleased" => "1902",
-		"purchaseUrl" => "http.example.com"
-
-	],
-
-]; 	
-
-
-$filteredbooks = array_filter($books, function ($book) {
-
-	return $book['category'] === 'Arts';
-
-});
-
 
 require 'index.view.php';
 
