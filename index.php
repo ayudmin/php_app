6b9ef42792ee0bd8393d2525511d412627f11744
francis556 <vikrammehra244@gmail.com>
2022-11-05 07:07:40 +0200

    working with PDO

--- index.php
+++ index.php
@@ -3,4 +3,21 @@
 
 require "functions.php";
 
-require "router.php";
+// require "router.php";
+
+// connect to our Mysql database.
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'francis','mansnothot');
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($posts as $post) {
+
+	echo "<li>" . $post['title'] . "</l>";
+}
