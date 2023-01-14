549cdd8326a7f1c092d8ac5a930fe9c0bad1781d
francis556 <vikrammehra244@gmail.com>
2023-01-14 09:29:16 +0200

    render notes and notes page

---
+++ controllers/notes.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = require('config.php');
+
+$db = new Databse($config['database']);
+
+$heading = "My Notes";
+
+$notes = $db->query('select * from notes where user_id = 4')->fetchAll();
+
+require 'views/notes.view.php';
+
+
--- index.php
+++ index.php
@@ -5,17 +5,8 @@ require "functions.php";
 
 require "Database.php";
 
-// require "router.php";
+require "router.php";
 
-$config = require('config.php');
 
-$db = new Databse($config['database']);
 
-$id = $_GET['id'];
-
-$query = "select * from posts where id = ? ";
-
-$posts = $db->query($query, [$id])->fetch();
-
-dd($posts);
 
