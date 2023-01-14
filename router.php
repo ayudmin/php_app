3a748c951d391ea9e3874632e370eed2557abe77
francis556 <vikrammehra244@gmail.com>
2023-01-14 09:28:40 +0200

    notes router

--- router.php
+++ router.php
@@ -5,44 +5,28 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $routes = [
 
 	'/' => 'controllers/index.php',
-
 	'/projects' => 'controllers/projects.php',
-
+	'/notes' => 'controllers/notes.php',
 	'/reports' => 'controllers/reports.php',
 
 ];
 
-
-
 function routeTocontroller ($uri, $routes)
-
 {
-
 	if (array_key_exists($uri, $routes))
-
 	{
-
 		require $routes[$uri];
-
 	} else {
-
 		abort();
 	}
 }
 
-
-
 function abort($code = 404)
-
 {
-
 	http_response_code($code);
-
 	require "views/templates/{$code}.php";
-
 	die();
 }
-
 
 routeTocontroller($uri, $routes);
 
