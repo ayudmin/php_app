b0ccd58fee3e2f93bc979554e0030e7064e30401
francis556 <vikrammehra244@gmail.com>
2023-01-21 07:11:08 +0200

    rendering appropriate error

--- controllers/note.php
+++ controllers/note.php
@@ -6,10 +6,17 @@ $db = new Databse($config['database']);
 
 $heading = "Note";
 
-$note = $db->query('select * from notes where user_id = :user and id = :id', [
-    'user' => 4,
+$note = $db->query('select * from notes where  id = :id', [
     'id'=> $_GET['id']
 ])->fetch();
+
+if(! $note){
+    abort();
+}
+
+if ($note['user_id'] != 4){
+    abort(403);
+}
 
 require 'views/note.view.php';
 
