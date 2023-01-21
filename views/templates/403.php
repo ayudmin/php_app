3855e2432967203d468975041d803e7503d9909d
francis556 <vikrammehra244@gmail.com>
2023-01-21 07:11:42 +0200

    added 403 page

---
+++ views/templates/403.php
@@ -0,0 +1,13 @@
+<?php require('views/templates/header.php') ?>
+<?php require('views/templates/nav.php') ?>
+
+<main>
+<div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    <h1 class="text-2xl font-bold">Sorry Authorisation Required.</h1>
+    <p class="mt-4">
+        <a href="/" class="text-blue underline">Go Back Home.</a>
+    </p>
+</div>
+</main>
+
+<?php require('views/templates/footer.php') ?>
