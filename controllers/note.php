ff850531c20a2ad0cbba362797af658b1f1f6418
francis556 <vikrammehra244@gmail.com>
2023-01-21 07:03:21 +0200

    limiting access using user id

--- controllers/note.php
+++ controllers/note.php
@@ -6,7 +6,10 @@ $db = new Databse($config['database']);
 
 $heading = "Note";
 
-$note = $db->query('select * from notes where id = :id', ['id'=> $_GET['id']])->fetch();
+$note = $db->query('select * from notes where user_id = :user and id = :id', [
+    'user' => 4,
+    'id'=> $_GET['id']
+])->fetch();
 
 require 'views/note.view.php';
 
