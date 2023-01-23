ddd45e6d61e07c5467128a85ccca8ae486db66c2
francis556 <vikrammehra244@gmail.com>
2023-01-23 08:59:48 +0200

    authorize function

--- functions.php
+++ functions.php
@@ -4,27 +4,22 @@
 function dd($value)
 
 {
-
 	echo "<pre>";
-
 	var_dump($value);
-
 	echo "</pre>";
-
 	die();
-
-
 }
-
 
 function urlIs($value)
-
 {
-
 	require $_SERVER['REQUEST_URI'] === $value;
-
 }
 
-
+function authorize($conditon)
+{
+	if(! $conditon){
+		abort(Response::FORBIDDEN);
+	}
+}
 
 
