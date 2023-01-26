9e1121ed8dce3f88f6e8e694ecc464d97e277601
francis556 <vikrammehra244@gmail.com>
2023-01-26 22:55:16 +0200

    separate routes from touter

---
+++ routes.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    '/' => 'controllers/index.php',
+    '/projects' => 'controllers/projects.php',
+    '/notes' => 'controllers/notes.php',
+    '/note' => 'controllers/note.php',
+    '/reports' => 'controllers/reports.php',
+
+];
