07cd2dd4638d6785af113acc3f981d0b9f6cdd6d
francis556 <vikrammehra244@gmail.com>
2023-02-04 22:04:17 +0200

    insert into db

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,15 @@
 <?php
 
+$config = require('config.php');
+$db = new Databse($config['database']);
+
 $heading = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    dd($_POST);
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+    ]);
 }
 require 'views/note-create.view.php';
 
