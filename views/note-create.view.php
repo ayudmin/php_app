13b1338c81e0716585b8bfe2bfe88e14523b9710
francis556 <vikrammehra244@gmail.com>
2023-02-04 15:42:06 +0200

    added a form

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -4,7 +4,12 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <h1>Create A Note</h1>
+        <form>
+            <textarea name="body"></textarea>
+            <p>
+                <button type="submit">Create Post</button>
+            </p>
+        </form>
     </div>
 </main>
 
