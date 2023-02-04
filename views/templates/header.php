1e3112352fbcc7922b6a4d570e9d56ffaecd3041
francis556 <vikrammehra244@gmail.com>
2023-02-04 16:22:09 +0200

    tailwindui form plugin

--- views/templates/header.php
+++ views/templates/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title></title>
-	<script src="https://cdn.tailwindcss.com"></script>
+	<script src="https://cdn.tailwindcss.com?plugins=forms"></script>
 </head>
 <body class="h-full">
 	<div class="min-h-full">
