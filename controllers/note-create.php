3bd55f99f557d4eb2d72db799e010a331a0ede04
francis556 <vikrammehra244@gmail.com>
2023-02-06 08:09:12 +0200

    form validator

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,19 @@ $db = new Databse($config['database']);
 $heading = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+    $errors = [];
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body text is required';
+    }
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body can not be more than 1000 characters.';
+    }
+    if (empty($errors)){
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 require 'views/note-create.view.php';
 
