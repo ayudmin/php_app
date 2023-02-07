6d5a0ccb84e358dc04f1ed8b543d23062e09f0fd
francis556 <vikrammehra244@gmail.com>
2023-02-07 08:14:26 +0200

    refactor to single function to validate

--- Validator.php
+++ Validator.php
@@ -2,8 +2,9 @@
 
 class Validator
 {
-    public function string($value)
+    public function string($value, $min = 1, $max = INF)
     {
-        return strlen(trim($value)) === 0;
+        $value = trim($value);
+        return strlen($value) >= $min && strlen($value) <= $max;
     }
 }
--- controllers/note-create.php
+++ controllers/note-create.php
@@ -10,11 +10,8 @@ $heading = "Create Note";
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $errors = [];
     $validator = new Validator();
-    if ($validator->string($_POST['body'])) {
-        $errors['body'] = 'A body text is required';
-    }
-    if (strlen($_POST['body']) > 1000) {
-        $errors['body'] = 'The body can not be more than 1000 characters.';
+    if (! $validator->string($_POST['body'], 1 , 1000)) {
+        $errors['body'] = 'A body of no more than 1000 characters is required';
     }
     if (empty($errors)){
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
