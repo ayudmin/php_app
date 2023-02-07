527a6524335a1c0bda1908692e9d68bb907b3742
francis556 <vikrammehra244@gmail.com>
2023-02-07 08:17:37 +0200

    refactor to static methods

--- Validator.php
+++ Validator.php
@@ -2,7 +2,7 @@
 
 class Validator
 {
-    public function string($value, $min = 1, $max = INF)
+    public static function string($value, $min = 1, $max = INF)
     {
         $value = trim($value);
         return strlen($value) >= $min && strlen($value) <= $max;
--- controllers/note-create.php
+++ controllers/note-create.php
@@ -9,8 +9,7 @@ $heading = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $errors = [];
-    $validator = new Validator();
-    if (! $validator->string($_POST['body'], 1 , 1000)) {
+    if (! Validator::string($_POST['body'], 1 , 1000)) {
         $errors['body'] = 'A body of no more than 1000 characters is required';
     }
     if (empty($errors)){
