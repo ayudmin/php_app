7ac8da700887cb793e75522c3507ac6ecc157919
francis556 <vikrammehra244@gmail.com>
2023-02-15 22:43:14 +0200

    overiding default POST method with delete

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -12,6 +12,7 @@
 
     <p><?= htmlspecialchars($note['body']) ?></p>
     <form class="mt-6" method="POST">
+        <input type="hidden" name="_method" value="DELETE">
         <input type="hidden" value="<?= $note['id'] ?>" name="id">
         <button class="text-sm text-red-500">Delete</button>
     </form>
