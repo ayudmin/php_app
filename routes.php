100ec1d346a45d3d972f9460994b880514ba4e59
francis556 <vikrammehra244@gmail.com>
2023-02-15 22:44:05 +0200

    building a better router

--- routes.php
+++ routes.php
@@ -1,12 +1,10 @@
 <?php
 
-return [
+$router->get('/', 'controllers/index.php');
+$router->get('/projects', 'controllers/projects.php');
+$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/note', 'controllers/notes/show.php');
+$router->get('/notes/create', 'controllers/notes/create.php');
+$router->get('/reports', 'controllers/reports.php');
+$router->delete('/note', 'controllers/notes/destroy.php');
 
-    '/' => 'controllers/index.php',
-    '/projects' => 'controllers/projects.php',
-    '/notes' => 'controllers/notes/index.php',
-    '/note' => 'controllers/notes/show.php',
-    '/notes/create' => 'controllers/notes/create.php',
-    '/reports' => 'controllers/reports.php',
-
-];
