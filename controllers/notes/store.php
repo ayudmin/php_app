5fedc71cf176938c38ec80f5307da21f38af5b43
francis556 <vikrammehra244@gmail.com>
2023-02-16 07:37:34 +0200

    save  action

---
+++ controllers/notes/store.php
@@ -0,0 +1,28 @@
+<?php
+
+use Core\Database;
+use Core\Validator;
+
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+$errors = [];
+
+if (! Validator::string($_POST['body'], 1 , 1000)) {
+    $errors['body'] = 'A body of no more than 1000 characters is required';
+}
+
+if (! empty($errors)){
+    return view('notes/create.view.php', [
+    'heading' => $heading,
+    'errors' => $errors
+    ]);
+}
+
+$db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+    'body' => $_POST['body'],
+    'user_id' => 1
+]);
+header('location: /notes');
+die();
+
