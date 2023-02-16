f55678c0a18edc5aec8809a2e45f9889329f4434
francis556 <vikrammehra244@gmail.com>
2023-02-16 07:40:08 +0200

    more uri endpoints

--- routes.php
+++ routes.php
@@ -2,9 +2,12 @@
 
 $router->get('/', 'controllers/index.php');
 $router->get('/projects', 'controllers/projects.php');
+$router->get('/reports', 'controllers/reports.php');
+
 $router->get('/notes', 'controllers/notes/index.php');
 $router->get('/note', 'controllers/notes/show.php');
+$router->delete('/note', 'controllers/notes/destroy.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
-$router->get('/reports', 'controllers/reports.php');
+$router->post('/notes', 'controllers/notes/store.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
 
