f31fcb596a0f042d1d8b923568852c7b475a64bb
francis556 <vikrammehra244@gmail.com>
2023-02-16 07:38:05 +0200

    destroy template

---
+++ views/notes/destroy.view.php
@@ -0,0 +1,22 @@
+<?php require base_path('views/templates/header.php') ?>
+<?php require base_path('views/templates/nav.php') ?>
+<?php require base_path('views/templates/banner.php') ?>
+
+<main>
+<div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    <p class="mb-6">
+        <a href="/notes" class="text-blue-500">
+            <button class="underline" >Go back</button>
+        </a>
+    </p>
+
+    <p><?= htmlspecialchars($note['body']) ?></p>
+    <form class="mt-6" method="POST">
+        <input type="hidden" name="_method" value="DELETE">
+        <input type="hidden" value="<?= $note['id'] ?>" name="id">
+        <button class="text-sm text-red-500">Delete</button>
+    </form>
+</div>
+</main>
+
+<?php require base_path('views/templates/footer.php') ?>
