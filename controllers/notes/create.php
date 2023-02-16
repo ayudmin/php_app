4efad0f34f941acd23cf0040419a543d7abec6a1
francis556 <vikrammehra244@gmail.com>
2023-02-16 07:36:48 +0200

    update form action

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,30 +1,11 @@
 <?php
-use Core\Database;
 use Core\Validator;
 
-
-$config = require base_path('config.php');
-$db = new Database($config['database']);
-
 $heading = "Create Note";
-
 $errors = [];
 
 if (! Validator::email('done@gmail.com')){
     dd('Not a valid email');
-}
-
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    if (! Validator::string($_POST['body'], 1 , 1000)) {
-        $errors['body'] = 'A body of no more than 1000 characters is required';
-    }
-    if (empty($errors)){
-        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-            'body' => $_POST['body'],
-            'user_id' => 1
-        ]);
-    }
 }
 
 view('notes/create.view.php', [
