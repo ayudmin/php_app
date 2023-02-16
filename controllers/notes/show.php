749335d5c20cd0a7137884fc7ba19ba1eb84fda5
francis556 <vikrammehra244@gmail.com>
2023-02-16 07:39:50 +0200

    update show a note

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -6,34 +6,18 @@ $config = require base_path('config.php');
 
 $db = new Database($config['database']);
 
-$currentUser = 25;
+$currentUser = 1;
 
 
 $heading = "Note";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $note = $db->query('select * from notes where  id = :id', [
-            'id'=> $_GET['id']
-    ])->findOrFail();
+$note = $db->query('select * from notes where  id = :id', [
+        'id'=> $_GET['id']
+])->findOrFail();
 
-    authorize($note['user_id'] === $currentUser);
-    $db->query('delete from notes where id = :id', [
-        'id' => $_GET['id']
-    ]);
-    header('location: /notes');
-    exit();
-} else {
-
-        $note = $db->query('select * from notes where  id = :id', [
-            'id'=> $_GET['id']
-        ])->findOrFail();
-
-        authorize($note['user_id'] === $currentUser);
-
-        view('notes/show.view.php', [
-            'heading' => $heading,
-            'note' => $note
-        ]);
-
-}
+authorize($note['user_id'] === $currentUser);
+view("notes/show.view.php", [
+    'heading' => 'Note',
+    'note' => $note
+]);
 
