147110f92e1385336da4e9ea397175ff843ea29f
francis556 <vikrammehra244@gmail.com>
2023-02-18 08:13:32 +0200

    service container

--- Core/Container.php
+++ Core/Container.php
@@ -5,10 +5,12 @@ namespace Core;
 class Container
 {
     protected $bindings = [];
+
     public function bind($key, $resolver)
     {
         $this->bindings[$key] = $resolver;
     }
+
     public function resolve($key)
     {
         if (! array_key_exists($key, $this->bindings)){
