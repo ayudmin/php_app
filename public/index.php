4fcf32f5b61f89239d281edcc1fc42790dae9258
francis556 <vikrammehra244@gmail.com>
2023-02-18 08:14:02 +0200

    service container

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ spl_autoload_register(function ($class){
     require base_path("{$class}.php");
 });
 
-require base_path('boorstrap.php');
+require base_path('bootstrap.php');
 
 $router = new \Core\Router();
 $routes = require base_path('routes.php');
