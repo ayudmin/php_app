b68db8ca90f9eb89c7e95e4e849f490b7589b7a2
francis556 <vikrammehra244@gmail.com>
2023-02-24 07:11:27 +0200

    update note showing

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -11,12 +11,15 @@
     </p>
 
     <p><?= htmlspecialchars($note['body']) ?></p>
+    <footer class="mt-6">
+        <a href="/note/edit" class="text-gray-500 border border-current px-3 py-1 rounded">Edit</a>
+    </footer>
+</div>
+</main>
+
+<?php require base_path('views/templates/footer.php') ?>
     <form class="mt-6" method="POST">
         <input type="hidden" name="_method" value="DELETE">
         <input type="hidden" value="<?= $note['id'] ?>" name="id">
         <button class="text-sm text-red-500">Delete</button>
     </form>
-</div>
-</main>
-
-<?php require base_path('views/templates/footer.php') ?>
