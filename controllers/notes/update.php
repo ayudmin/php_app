66883d192d068281aff885c57470f215dc5e91c3
francis556 <vikrammehra244@gmail.com>
2023-02-24 08:13:06 +0200

    update controller

---
+++ controllers/notes/update.php
@@ -0,0 +1,36 @@
+<?php
+
+use Core\Database;
+use Core\App;
+use Core\Validator;
+
+$heading = "Update Note";
+$errors = [];
+$db = App::resolve(Database::class);
+$currentUser = 4;
+
+$note = $db->query('select * from notes where  id = :id', [
+        'id'=> $_POST['id']
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUser);
+if (! Validator::string($_POST['body'], 1 , 1000)) {
+    $errors['body'] = 'A body of no more than 1000 characters is required';
+}
+
+if (count($errors)){
+    return view('notes/edit.view.php', [
+        'heading' => 'Edit Note',
+        'errors' => $errors,
+        'note' => $note
+    ]);
+}
+
+$db->query('update notes set body = :body where id = :id', [
+    'id' => $_POST['id'],
+    'body' => $_POST['body']
+]);
+
+header('location: /notes');
+die();
+
