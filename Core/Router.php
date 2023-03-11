7fa4f0486f5e423815129fdc416f4bbe01a03903
francis556 <vikrammehra244@gmail.com>
2023-03-11 06:27:15 +0200

    binding middleware with routes defaults

--- Core/Router.php
+++ Core/Router.php
@@ -8,29 +8,37 @@ class Router {
 		$this->routes[] = [
 			'uri' => $uri,
 			'controller' => $controller,
-			'method' => $method
+			'method' => $method,
+			'middleware' => null
 		];
+		return $this;
 	}
 
 	public function get($uri, $controller)
 	{
-		$this->add('GET', $uri, $controller);
+		return $this->add('GET', $uri, $controller);
 	}
 	public function post($uri, $controller)
 	{
-		$this->add('POST', $uri, $controller);
+		return $this->add('POST', $uri, $controller);
 	}
 	public function delete($uri, $controller)
 	{
-		$this->add('DELETE', $uri, $controller);
+		return $this->add('DELETE', $uri, $controller);
 	}
 	public function patch($uri, $controller)
 	{
-		$this->add('PATCH', $uri, $controller);
+		return $this->add('PATCH', $uri, $controller);
 	}
 	public function put($uri, $controller)
 	{
-		$this->add('PUT', $uri, $controller);
+		return $this->add('PUT', $uri, $controller);
+	}
+
+	public function only($key)
+	{
+		$this->routes[array_key_last($this->routes)]['middleware'] = $key;
+		$return $this;
 	}
 	public function route($uri, $method)
 	{
