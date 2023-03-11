26d3e93619ffc74688ffce31f94ec2867cf32e90
francis556 <vikrammehra244@gmail.com>
2023-03-11 07:28:27 +0200

    code: middleware stuff hahah just following

---
+++ Core/Middleware/Middleware.php
@@ -0,0 +1,21 @@
+<?php 
+namespace Core\Middleware;
+
+class Middleware 
+{
+	public const MAP =[
+		'guest' => Guest::class,
+		'auth' => Auth::class
+	];
+	public static function resolve($key)
+	{
+		if (! $key){
+			return;
+		}
+		$middleware = static::MAP[$key] ?? false;
+		if (! $middleware){
+			throw new \Exception("No matching middleware found for key {$key} .");
+		}
+		(new $middleware)->handle();
+	}
+}
--- Core/Router.php
+++ Core/Router.php
@@ -1,5 +1,8 @@
 <?php 
 namespace Core;
+use Core\Middleware\Auth;
+use Core\Middleware\Guest;
+use Core\Middleware\Middleware;
 
 class Router {
 	protected $routes = [];
@@ -44,12 +47,7 @@ class Router {
 	{
 		foreach ($this->routes as $route) {
 			if($route['uri'] === $uri && $route['method'] === strtoupper($method)){
-				if ($route['middleware'] === 'guest') {
-					if ($_SESSION['user'] ?? false){
-						header('location: /');
-						exit();
-					}
-				}
+				Middleware::resolve($route['middleware']);
 				return require base_path($route['controller']);
 			}
 		}
