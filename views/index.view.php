004341ae82192ae9c5039cc0a7f72910bdf0c961
francis556 <vikrammehra244@gmail.com>
2023-03-14 07:17:00 +0200

    info: hellowing user

--- views/index.view.php
+++ views/index.view.php
@@ -4,7 +4,7 @@
 
 <main>
 <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-<p>Hello, world home page.</p>
+<p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?> Welcome back.</p>
 </div>
 </main>
 
